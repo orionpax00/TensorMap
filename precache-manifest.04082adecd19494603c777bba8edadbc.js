997125494a81b3975c62188365cef134c95326ff
Durgesh <durgesh123.iitr@gmail.com>
2020-07-05 19:19:17 +0000

    Updates

---
+++ precache-manifest.04082adecd19494603c777bba8edadbc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fcaa49046920d61cc691df8b6a7cb55",
+    "url": "/Tensormap/index.html"
+  },
+  {
+    "revision": "75eac51760261411c271",
+    "url": "/Tensormap/static/css/main.cd486b66.chunk.css"
+  },
+  {
+    "revision": "99066f335bc67c2574b7",
+    "url": "/Tensormap/static/js/2.37e9e96a.chunk.js"
+  },
+  {
+    "revision": "75eac51760261411c271",
+    "url": "/Tensormap/static/js/main.2e4180fd.chunk.js"
+  },
+  {
+    "revision": "1726221056ad56acaa5f",
+    "url": "/Tensormap/static/js/runtime~main.1076447c.js"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/Tensormap/static/media/GitHubicon.76d344f0.svg"
+  },
+  {
+    "revision": "2f608422b21b6b59a07f7869b19e96e5",
+    "url": "/Tensormap/static/media/upload.2f608422.png"
+  }
+]);
